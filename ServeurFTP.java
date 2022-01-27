5e773b84b42f070dc019245256f2eee5c8b4726c
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2022-01-27 20:11:48 +0100

    Add files via upload

--- ServeurFTP.java
+++ ServeurFTP.java
@@ -9,7 +9,6 @@ import java.net.Socket;
 public class ServeurFTP {
 	
 	private ServerSocket serverSock;
-	public boolean isServerRuning;
 	public int port;
 	
 	public ServeurFTP(int port) {
@@ -18,7 +17,6 @@ public class ServeurFTP {
 			this.port = port;
 			InetSocketAddress addr = new InetSocketAddress(this.port);
 			this.serverSock.bind(addr);
-			this.isServerRuning = true;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -26,7 +24,7 @@ public class ServeurFTP {
 	
 	public void listen() {
 		System.out.println("Listening on port "+this.serverSock.getLocalPort());
-		while(isServerRuning) {
+		while(true) {
 			Socket socket;
 			try {
 				socket = this.serverSock.accept();
